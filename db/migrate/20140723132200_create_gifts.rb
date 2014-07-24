7dc561a6b19e502766ef3c5845e4e9fcff60fed1
Ryan Menzer <ryan.c.menzer@gmail.com>
2014-07-24 16:38:18 -0400

    correcting spelling error

--- db/migrate/20140723132200_create_gifts.rb
+++ db/migrate/20140723132200_create_gifts.rb
@@ -2,7 +2,7 @@ class CreateGifts < ActiveRecord::Migration
   def change
     create_table :gifts do |t|
       t.integer :appeal_id, null: false
-      t.integer :amound, null: false
+      t.integer :amount, null: false
       t.string :donor, null: false
       t.timestamps
     end
